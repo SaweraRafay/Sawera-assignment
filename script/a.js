38301b9e4c9029a14dd7ee5aebcc7394b762a9b6
SaweraRafay <saweraaqeel@gmail.com>
2023-04-09 22:57:49 +0500

    Initialize Project

---
+++ script/a.js
@@ -0,0 +1,11 @@
+
+// alert("Sawera here");
+
+
+// task
+
+var zakat_percetage =0.025; 
+var user_input = +prompt("Enter Your Amount");
+var result = zakat_percetage * user_input;
+
+alert("your zakat value is " + result)
